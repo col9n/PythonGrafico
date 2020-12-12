9136b02e1e69c8ecabead69ba30437c7711ad17d
col9n <col9nrock13base@gmail.com>
2020-12-12 16:36:27 +0100

    2

--- main.py
+++ main.py
@@ -1,1 +1,84 @@
-print("hola sdas das dasd")
+# https://realpython.com/python-gui-tkinter/
+import tkinter as tk
+from tkinter.filedialog import askopenfilename, asksaveasfilename
+
+window = tk.Tk()
+
+frame1 = tk.Frame(master=window, width=200, height=100, bg="red")
+frame1.pack(fill=tk.BOTH, side=tk.LEFT, expand=True)
+
+frame2 = tk.Frame(master=window, width=100, bg="yellow")
+frame2.pack(fill=tk.BOTH, side=tk.LEFT, expand=True)
+
+frame3 = tk.Frame(master=window, width=50, bg="blue")
+frame3.pack(fill=tk.BOTH, side=tk.LEFT, expand=True)
+
+window.mainloop()
+
+#---------------------------------
+
+window1 = tk.Tk()
+
+for i in range(3):
+    window1.columnconfigure(i, weight=1, minsize=75)
+    window1.rowconfigure(i, weight=1, minsize=50)
+
+    for j in range(0, 3):
+        frame = tk.Frame(
+            master=window1,
+            relief=tk.RAISED,
+            borderwidth=1
+        )
+        frame.grid(row=i, column=j, padx=5, pady=5)
+
+        label = tk.Label(master=frame, text=f"Row {i}\nColumn {j}")
+        label.pack(padx=5, pady=5)
+
+window1.mainloop()
+
+
+#---------------------------------
+
+def open_file():
+    """Open a file for editing."""
+    filepath = askopenfilename(
+        filetypes=[("Text Files", "*.txt"), ("All Files", "*.*")]
+    )
+    if not filepath:
+        return
+    txt_edit.delete(1.0, tk.END)
+    with open(filepath, "r") as input_file:
+        text = input_file.read()
+        txt_edit.insert(tk.END, text)
+    window2.title(f"Simple Text Editor - {filepath}")
+
+def save_file():
+    """Save the current file as a new file."""
+    filepath = asksaveasfilename(
+        defaultextension="txt",
+        filetypes=[("Text Files", "*.txt"), ("All Files", "*.*")],
+    )
+    if not filepath:
+        return
+    with open(filepath, "w") as output_file:
+        text = txt_edit.get(1.0, tk.END)
+        output_file.write(text)
+    window2.title(f"Simple Text Editor - {filepath}")
+
+window2 = tk.Tk()
+window2.title("Simple Text Editor")
+window2.rowconfigure(0, minsize=800, weight=1)
+window2.columnconfigure(1, minsize=800, weight=1)
+
+txt_edit = tk.Text(window2)
+fr_buttons = tk.Frame(window2, relief=tk.RAISED, bd=2)
+btn_open = tk.Button(fr_buttons, text="Open", command=open_file)
+btn_save = tk.Button(fr_buttons, text="Save As...", command=save_file)
+
+btn_open.grid(row=0, column=0, sticky="ew", padx=5, pady=5)
+btn_save.grid(row=1, column=0, sticky="ew", padx=5)
+
+fr_buttons.grid(row=0, column=0, sticky="ns")
+txt_edit.grid(row=0, column=1, sticky="nsew")
+
+window2.mainloop()
